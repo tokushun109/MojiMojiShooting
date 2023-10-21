e8bd86ab969bdebd7d41e58914f6d5dedf584c28
tokushun109 <tokushun109@gmail.com>
2023-10-21 15:54:00 +0900

    #2 ゲーム開始時にオブジェクトが表示されるように実装

--- Assets/Scripts/Target/Property.cs
+++ Assets/Scripts/Target/Property.cs
@@ -4,5 +4,6 @@ namespace Target.Generator
 {
 	public partial class TargetGenerator : MonoBehaviour
 	{
+		[SerializeField, Header("ターゲットのゲームオブジェクト")] GameObject targetObj;
 	}
 }
--- Assets/Scripts/Target/TargetGenerator.cs
+++ Assets/Scripts/Target/TargetGenerator.cs
@@ -5,10 +5,11 @@ namespace Target.Generator
 	public partial class TargetGenerator : MonoBehaviour
 	{
 		// Start is called before the first frame update
-		// void Start()
-		// {
-
-		// }
+		void Start()
+		{
+			Application.targetFrameRate = 60;
+			Instantiate(targetObj, new Vector3(-1.35f, 1, 0), Quaternion.identity);
+		}
 
 		// Update is called once per frame
 		// void Update()
